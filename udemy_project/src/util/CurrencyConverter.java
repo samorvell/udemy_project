86d4e65d1633ac4d64630c89a4699afd8ac96ee9
samorvell <samuel_amaro_4@hotmail.com>
2021-05-05 21:35:10 -0300

    finalizado exercicio objetos estaticos

--- udemy_project/src/util/CurrencyConverter.java
+++ udemy_project/src/util/CurrencyConverter.java
@@ -3,7 +3,6 @@ package util;
 public class CurrencyConverter {
 	
 	
-	public static double  total;
 	public static final double IOF = 0.06;
 	
 	public static double amount(double price, double quantity) {
