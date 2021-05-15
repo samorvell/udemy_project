fa0e16daadc3a0c9cb79945b461ebfad06d60f0b
samorvell <samuel_amaro_4@hotmail.com>
2021-05-14 22:55:46 -0300

    finalização coneito this

--- udemy_project/src/aplication/ProductProgram.java
+++ udemy_project/src/aplication/ProductProgram.java
@@ -12,20 +12,23 @@ public class ProductProgram {
 		Locale.setDefault(Locale.US);//atualização localização
 		Scanner sc = new Scanner(System.in);//criação do scanner	
 		
-		Product product = new Product();		
+		//Product product = new Product(); essa estancia saí daqui, para ser criado após a informação das variaveis
+
 		System.out.println("Informe os dados do produto: ");
 		System.out.print("Nome: ");
-		product.name = sc.nextLine();
+		String name = sc.nextLine();
 		System.out.print("Preço: ");
-		product.price = sc.nextDouble();
+		double price = sc.nextDouble();
 		System.out.print("Quantide em estoque: ");
-		product.quantity = sc.nextInt();
+		int quantity = sc.nextInt();
+
+		Product product = new Product(name, price, quantity);
 		
 		System.out.println();
 		System.out.println("Dados do produto: " + product);
 		System.out.println();
 		System.out.print("Informa a quantidade de produtos a adicionar no estoque: ");
-		int quantity = sc.nextInt();
+		quantity = sc.nextInt(); //retirado o tipo da variavel, pois já foi informada anteriomente.
 		product.addProducts(quantity);
 		
 		System.out.println();
@@ -36,7 +39,7 @@ public class ProductProgram {
 		quantity = sc.nextInt();
 		product.removeProducts(quantity);
 		System.out.println();
-		System.out.println("Dados do produto atualizados: " + product); 
+		System.out.println("Dados do produto atualizados: " + product);
 		
 	
 		sc.close();
--- udemy_project/src/entities/Product.java
+++ udemy_project/src/entities/Product.java
@@ -6,6 +6,14 @@ public class Product {
 	public String name;
 	public double price;
 	public int quantity;
+
+	public Product(String name, double price, int quantity){ //esse construtor foi criado para forçar a incialização das variaveis com valores, para não correr risco de criar produtos sem atributos
+		this.name = name; // this. refere-se ao atributo da classe, sem o this. a variavel local
+		this.price = price;
+		this.quantity = quantity;
+
+
+	}
 	
 	public double totalValueInStock() {
 		return price * quantity;
